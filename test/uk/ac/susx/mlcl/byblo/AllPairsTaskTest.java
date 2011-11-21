aa2e52378073fcc72b2db3f68eaee2e00c01ad3c
Hamish Morgan <hamish.morgan@sussex.ac.uk>
2011-11-21 09:36:04 +0000

    Fixed AllPairsTaskTest.java to refernce a file that is actually distributed.

--- test/uk/ac/susx/mlcl/byblo/AllPairsTaskTest.java
+++ test/uk/ac/susx/mlcl/byblo/AllPairsTaskTest.java
@@ -1,21 +1,45 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2010-2011, University of Sussex
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Sussex nor the names of its 
+ *    contributors may be used to endorse or promote products derived from this 
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package uk.ac.susx.mlcl.byblo;
 
-import com.google.common.base.Objects.ToStringHelper;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import uk.ac.susx.mlcl.lib.test.ExitTrapper;
-import static org.junit.Assert.*;
 
 /**
  *
- * @author hiam20
+ * @author Hamish Morgan &lt;hamish.morgan@sussex.ac.uk&gt;
  */
 public class AllPairsTaskTest {
 
@@ -42,34 +66,10 @@ public class AllPairsTaskTest {
     public void testMainRun() throws Exception {
         ExitTrapper.enableExistTrapping();
         Main.main(new String[]{"allpairs",
-                    "-i", "/Volumes/Local Scratch HD/Local Home/Documents/Data/lm-medline/r2/lm-800k.dtt-tb-tf-cb-cf-cw-55.entry-features.filtered.sample",
-                    "-o", "/Volumes/Local Scratch HD/Local Home/Documents/Data/lm-medline/r2/lm-800k.dtt-tb-tf-cb-cf-cw-55.entry-features.sims",
+                    "-i", "testdata/fruit/bnc-gramrels-fruit.entryFeatures",
+                    "-o", "testdata/out/bnc-gramrels-fruit.out",
                     "-C", "500"});
         ExitTrapper.disableExitTrapping();
     }
-//    /**
-//     * Test of runCommand method, of class AllPairsTask.
-//     */
-//    @Test
-//    public void testRunCommand() throws Exception {
-//        System.out.println("runCommand");
-//        AllPairsTask instance = new AllPairsTask();
-//        instance.runCommand();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of toStringHelper method, of class AllPairsTask.
-//     */
-//    @Test
-//    public void testToStringHelper() {
-//        System.out.println("toStringHelper");
-//        AllPairsTask instance = new AllPairsTask();
-//        ToStringHelper expResult = null;
-//        ToStringHelper result = instance.toStringHelper();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+
 }
