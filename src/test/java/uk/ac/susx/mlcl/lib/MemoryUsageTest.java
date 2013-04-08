1f59c91830f7afc3286317e72bb1f82d8f3f6acd
Hamish Morgan <hamish.morgan@sussex.ac.uk>
2013-04-08 18:06:03 +0100

    Fixed bug in MemoryUsage unit test.

--- src/test/java/uk/ac/susx/mlcl/lib/MemoryUsageTest.java
+++ src/test/java/uk/ac/susx/mlcl/lib/MemoryUsageTest.java
@@ -273,7 +273,6 @@ public class MemoryUsageTest {
         final long expectedSize = mp.getSizeBits();
 
         for (int i = 0; i < 10000; i++) {
-            mp = new MemoryUsage();
             mp.add(cal);
             assertEquals(expectedSize, mp.getSizeBits());
         }
